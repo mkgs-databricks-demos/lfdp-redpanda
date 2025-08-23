0841b14ec6b7cf10367194db891e7b331f0d91c1
matthew-gigl-db <matthew-gigl-db>
2025-08-23 11:05:33 +0000

    add profiles topic

---
+++ rp_cloud_quickstart/resources/rp_cloud_quickstart_pipeline/transformations/profiles.py
@@ -0,0 +1,26 @@
+import dlt
+from pyspark.sql.functions import col
+from utilities import utils
+
+redpanda_config = utils.get_redpanda_config(spark = spark, dbutils = dbutils)
+
+@dlt.table
+def hello_world():
+    """
+    Read Stream from the Redpanda Enterprise Quickstart "logins" topic. 
+    """
+    return (
+        spark.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers",redpanda_config.get("bootstrap.servers"))
+        .option("subscribe", "profiles")
+        .option("kafka.sasl.mechanism", redpanda_config.get("sasl.mechanism"))
+        .option("kafka.security.protocol", redpanda_config.get("security.protocol"))
+        .option("kafka.sasl.jaas.config", redpanda_config.get("sasl.jaas.config"))
+        # Optional: Set failOnDataLoss to false to avoid query failure if data is missing
+        .option("failOnDataLoss", "false")
+        # Optional: Set startingOffsets to earliest for initial testing
+        .option("startingOffsets", "earliest")
+        .load()
+        .withColumn("value_str", col("value").cast("string"))
+    )
