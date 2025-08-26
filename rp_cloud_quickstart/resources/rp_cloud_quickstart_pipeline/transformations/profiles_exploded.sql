dab247deb7ce64e5f8beeea43827c024356588bf
matthew-gigl-db <matthew-gigl-db>
2025-08-26 19:39:59 +0000

    another example commented out

--- rp_cloud_quickstart/resources/rp_cloud_quickstart_pipeline/transformations/profiles_exploded.sql
+++ rp_cloud_quickstart/resources/rp_cloud_quickstart_pipeline/transformations/profiles_exploded.sql
@@ -26,3 +26,29 @@ FROM (
   FROM STREAM(profiles_bronze)
   SELECT *, parse_json(value_str) as variant_col
 );
+
+
+-- CREATE STREAMING TABLE profiles_example
+-- TBLPROPERTIES (
+--   'delta.enableChangeDataFeed' = 'true',
+--   'delta.enableDeletionVectors' = 'true',
+--   'delta.enableRowTracking' = 'true',
+--   'delta.feature.variantType-preview' = 'supported',
+--   'quality' = 'bronze'
+-- )
+-- AS 
+-- SELECT 
+--   topic
+--   ,partition
+--   ,offset
+--   ,timestamp
+--   ,timestampType
+--   ,ingestTime
+--   ,t1.pos as profile_pos
+--   ,t1.key as profile_key
+--   ,t1.value as profile_value
+-- FROM (
+--   FROM STREAM(profiles_bronze)
+--   SELECT *, parse_json(value_str) as variant_col
+-- )
+-- ,LATERAL variant_explode(variant_col) as t1;
