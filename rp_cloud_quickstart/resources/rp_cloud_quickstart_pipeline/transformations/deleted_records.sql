61fd883129051477bf334d44bc9db67a61c822ba
matthew-gigl-db <matthew-gigl-db>
2025-08-28 11:44:53 +0000

    new streaming tables and materialized views to show deleted records and total deleted record counts

--- rp_cloud_quickstart/resources/rp_cloud_quickstart_pipeline/transformations/deleted_records.sql
+++ rp_cloud_quickstart/resources/rp_cloud_quickstart_pipeline/transformations/deleted_records.sql
@@ -4,5 +4,14 @@ CREATE FLOW deleted_records AS
 INSERT INTO deleted_records_from_profiles_bronze BY NAME 
 SELECT * FROM (
   FROM STREAM(profiles_cdf) |>
-  WHERE `_change_type` = 'delete'
+  WHERE (_change_type = 'delete') |>
+  SELECT *
+    ,_change_type AS change_type
+    ,_commit_version as commit_version
+    ,_commit_timestamp as commit_timestamp |>
+  SELECT * EXCEPT (_change_type, _commit_version, _commit_timestamp)
+);
+
+CREATE OR REFRESH MATERIALIZED VIEW mv_deleted_profile_rcrd_cnt AS (
+  SELECT COUNT(*) AS cnt FROM deleted_records_from_profiles_bronze
 );
