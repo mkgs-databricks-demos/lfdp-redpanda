d23720f9b89a42617b49e0b0e2431f6cf341d41b
matthew-gigl-db <matthew-gigl-db>
2025-09-13 21:01:46 +0000

    updates for delta sinks

--- rp_cloud_quickstart/resources/rp_cloud_quickstart_pipeline/transformations/kafka_bronze.py
+++ rp_cloud_quickstart/resources/rp_cloud_quickstart_pipeline/transformations/kafka_bronze.py
@@ -1,52 +1,31 @@
-import dlt
-from pyspark.sql.functions import col, current_timestamp
+# import dlt
+# from pyspark.sql.functions import col, current_timestamp
 from utilities import utils
 
 redpanda_config = utils.get_redpanda_config(spark = spark, dbutils = dbutils)
 
-topics = ["profiles", "__redpanda.connect.status", "__redpanda.connect.logs"]
+# spark config 
+catalog_use = spark.conf.get("catalog_use")
+schema_use = spark.conf.get("schema_use")
+event_log = spark.conf.get("event_log")
+sink_catalog = spark.conf.get("sink_catalog")
+sink_schema = spark.conf.get("sink_schema")
+
+# topics = ["profiles"]
 
 # uncomment to see how to ingest multiple topics to bronze tables
-# topics = ["profiles", "hello-world", "__redpanda.connect.status", "__redpanda.connect.logs"]
-
-class Bronze:
-    def __init__(self, topic):
-        self.topic = topic
-
-    def topic_ingestion(self):
-
-        topic_name = self.topic.replace('-', '_').replace(".", "_")
-
-        @dlt.table(
-            name = f"{topic_name}_bronze"
-            ,table_properties={
-                'quality' : 'bronze'
-                ,'delta.enableChangeDataFeed' : 'true'
-                ,'delta.enableDeletionVectors' : 'true'
-                ,'delta.enableRowTracking' : 'true'
-            }
-        )
-        def profiles_bronze():
-            """
-            Read Stream from the Redpanda Enterprise Quickstart "logins" topic. 
-            """
-            return (
-                spark.readStream
-                .format("kafka")
-                .option("kafka.bootstrap.servers",redpanda_config.get("bootstrap.servers"))
-                .option("subscribe", self.topic)
-                .option("kafka.sasl.mechanism", redpanda_config.get("sasl.mechanism"))
-                .option("kafka.security.protocol", redpanda_config.get("security.protocol"))
-                .option("kafka.sasl.jaas.config", redpanda_config.get("sasl.jaas.config"))
-                # Optional: Set failOnDataLoss to false to avoid query failure if data is missing
-                .option("failOnDataLoss", "false")
-                # Optional: Set startingOffsets to earliest for initial testing
-                .option("startingOffsets", "earliest")
-                .load()
-                .withColumn("value_str", col("value").cast("string"))
-                .withColumn("ingestTime", current_timestamp())
-            )
+topics = ["profiles", "hello-world", "__redpanda.connect.status", "__redpanda.connect.logs"]
 
 for topic in topics:
-    kakfa_ingest = Bronze(topic)
+    kakfa_ingest = utils.Bronze(
+        spark = spark
+        ,topic = topic
+        ,catalog = catalog_use
+        ,schema = schema_use
+        ,event_log = event_log
+        ,sink_catalog = sink_catalog
+        ,sink_schema = sink_schema
+        ,redpanda_config = redpanda_config
+    )
     kakfa_ingest.topic_ingestion()
+    kakfa_ingest.backfill_full_refresh()
--- rp_cloud_quickstart/resources/rp_cloud_quickstart_pipeline/utilities/utils.py
+++ rp_cloud_quickstart/resources/rp_cloud_quickstart_pipeline/utilities/utils.py
@@ -1,4 +1,5 @@
-from pyspark.sql.functions import udf
+import dlt
+from pyspark.sql.functions import col, current_timestamp, lit, udf
 from pyspark.sql.types import FloatType
 from pyspark.sql import SparkSession
 from typing import Any
@@ -22,3 +23,79 @@ def get_redpanda_config(spark: SparkSession, dbutils: Any) -> dict:
         "sasl.mechanism": "SCRAM-SHA-256",
         "sasl.jaas.config": f"kafkashaded.org.apache.kafka.common.security.scram.ScramLoginModule required username='{dbutils.secrets.get(scope=secret_scope, key=secret_key_user)}' password='{dbutils.secrets.get(scope=secret_scope, key=secret_key_password)}';"
     }
+
+class Bronze:
+    def __init__(self, spark: SparkSession, topic: str, catalog: str, schema: str, sink_catalog: str, sink_schema: str, event_log: str, redpanda_config: dict):
+        self.spark = spark
+        self.topic = topic
+        self.catalog = catalog
+        self.schema = schema
+        self.event_log = event_log
+        self.sink_catalog = sink_catalog
+        self.sink_schema = sink_schema
+        self.redpanda_config = redpanda_config
+        self.topic_name = self.topic.replace('-', '_').replace(".", "_")
+
+    def topic_ingestion(self):
+
+        @dlt.table(
+            name = f"{self.topic_name}_bronze"
+            ,table_properties={
+                'quality' : 'bronze'
+                ,'delta.enableChangeDataFeed' : 'true'
+                ,'delta.enableDeletionVectors' : 'true'
+                ,'delta.enableRowTracking' : 'true'
+                ,'delta.autoOptimize.optimizeWrite': 'true'
+                ,'delta.autoOptimize.autoCompact': 'true'
+            }
+        )
+        def kafka_bronze():
+            """
+            Read Stream from the Redpanda Enterprise Quickstart "logins" topic. 
+            """
+            return (
+                self.spark.readStream
+                .format("kafka")
+                .option("kafka.bootstrap.servers",self.redpanda_config.get("bootstrap.servers"))
+                .option("subscribe", self.topic)
+                .option("kafka.sasl.mechanism", self.redpanda_config.get("sasl.mechanism"))
+                .option("kafka.security.protocol", self.redpanda_config.get("security.protocol"))
+                .option("kafka.sasl.jaas.config", self.redpanda_config.get("sasl.jaas.config"))
+                # Optional: Set failOnDataLoss to false to avoid query failure if data is missing
+                .option("failOnDataLoss", "false")
+                # Optional: Set startingOffsets to earliest for initial testing
+                .option("startingOffsets", "earliest")
+                .load()
+                .withColumn("value_str", col("value").cast("string"))
+                .withColumn("ingestTime", current_timestamp())
+            )
+
+        dlt.create_sink(
+            name = f"{self.topic_name}_bronze_delta_sink" 
+            ,format = "delta"
+            ,options={
+                "tableName": f"{self.sink_catalog}.{self.sink_schema}.{self.topic_name}_kafka_delta_sink",
+                "quality": "bronze",
+                "delta.autoOptimize.optimizeWrite": "true",
+                "delta.autoOptimize.autoCompact": "true"
+            }
+        )
+
+        @dlt.append_flow(
+            name = f"flow_{self.topic_name}_bronze_delta_sink"
+            ,target = f"{self.topic_name}_bronze_delta_sink"
+        )
+        def delta_sink_flow():
+            return self.spark.readStream.table(f"{self.topic_name}_bronze") 
+        
+    def backfill_full_refresh(self):
+        @dlt.append_flow(
+            target = f"{self.topic_name}_bronze",
+            once = True,
+            name = f"flow_refresh_{self.topic_name}_bronze",
+            comment = f"Backfill data no longer available in kafka in bronze"
+        )
+        def backfill():
+            return (
+                self.spark.read.table(f"{self.sink_catalog}.{self.sink_schema}.{self.topic_name}_kafka_delta_sink")
+            )
